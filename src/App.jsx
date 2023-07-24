df556a334bf24710c89cfd75b9f38baa816b48c5
himanshu <himanshu92jain@gmail.com>
2023-07-24 20:54:40 +0530

    feat:layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import HomePage from './pages/HomePage'
 import LoginUserPage from './pages/LoginUserPage'
 import RegisterUserPage from './pages/RegisterUserPage'
 import  UserProfilePage from './pages/UserProfile'
+import MainLayout from './layouts/MainLayout'
 
 function App() {
   return (
     <Routes>
 
+      <Route path='/' element={<MainLayout/>}>
+
       <Route path='/' element={<HomePage />}/>
       <Route path='/login' element={<LoginUserPage />}/>
       <Route path='/register' element={<RegisterUserPage />}/>
       <Route path='/profile' element={<UserProfilePage />}/>
+
+      </Route>
       
     </Routes>
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Header from '../components/main/Header'
+import Footer from '../components/main/Footer'
+import { Outlet } from 'react-router-dom'
 function MainLayout() {
   return (
-    <div>
-      
-    </div>
+  <main>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+  </main>
   )
 }
 
